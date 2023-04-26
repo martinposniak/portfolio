77a755cb9d2c70cbcfece4662b6605584ad61d45
martin posniak <martin.posniak@hotmail.com>
2023-04-26 08:52:30 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } fr
 
 const App = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<Navbar />}>
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
